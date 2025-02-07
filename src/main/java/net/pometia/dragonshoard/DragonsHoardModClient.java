927a3d499fa8f2bbfcfb97c475fef6330848ae78
Pome-Tia <fangvotora@gmail.com>
2025-02-07 07:07:15 -0500

    Initial commit

---
+++ src/main/java/net/pometia/dragonshoard/DragonsHoardModClient.java
@@ -0,0 +1,10 @@
+package net.pometia.dragonshoard;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DragonsHoardModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
